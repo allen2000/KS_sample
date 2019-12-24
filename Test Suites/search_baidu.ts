f1e5b48b4e0be1c4791a63e4e470073e9ca4d6d1
Allen <lht2008@163.com>
2019-12-25 00:24:46 +0800

    init

---
+++ Test Suites/search_baidu.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>search_baidu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fe723914-d17a-42ed-87e7-03a8ff9a8f5d</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c33d8b3-0989-4942-9881-7fc89e63b491</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>581c37ae-6ed5-40ae-a1c2-6c4f1d19dbc4</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
