1fc36dcc26121822ed1c272c2d5e1c481ac85480
allen2000 <lht2008@163.com>
2020-10-03 14:03:35 +0800

    add appium protype

---
+++ Scripts/UI/Protype/APP_UI_Appium_calc/Script1601704856502.groovy
@@ -0,0 +1,30 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities; 
+import io.appium.java_client.android.AndroidDriver;
+
+DesiredCapabilities capabilities = new DesiredCapabilities();
+capabilities.setCapability("platformName", "Android");
+capabilities.setCapability("udid", "48629002");
+driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+driver.findElementByXPath('//android.widget.TextView[@content-desc="计算器"]').click()
+Thread.sleep(1000)
+driver.findElementById("com.android.calculator2:id/digit_8").click()
+driver.navigate().back()
+driver.navigate().back()
+driver.quit()
