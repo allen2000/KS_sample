53e901c7e2a8054c54de03daa54692783ea8d42a
AH <lht2008@163.com>
2021-01-09 19:40:24 +0800

    up the mi_calc show_result in OBR

--- Object Repository/mi_calc0/result_show.rs
+++ Object Repository/mi_calc0/result_show.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//hierarchy/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/com.miui.support.view.ViewPager[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[1]/android.widget.TextView[1][count(. | //*[@class = 'android.widget.TextView' and @instance = '3' and @resource-id = 'com.miui.calculator:id/result']) = count(//*[@class = 'android.widget.TextView' and @instance = '3' and @resource-id = 'com.miui.calculator:id/result'])]</value>
+         <value>//*[@class = 'android.widget.TextView' and @resource-id = 'com.miui.calculator:id/result']</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -20,7 +20,7 @@
       <value>android.widget.TextView</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>instance</name>
       <type>Main</type>
@@ -146,7 +146,7 @@
       <value>188</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
